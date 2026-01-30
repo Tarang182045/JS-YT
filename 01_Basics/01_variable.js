dcfd70dcd07935907bccd5517eff78842affdfd7
Tarang Tank <167697814+Tarang182045@users.noreply.github.com>
2026-01-30 23:10:08 +0530

    Create 01_variable.js

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountid=11111
+let accountEmail="tarang@gool.com"
+var accountPassword="12344"
+accountCity= "mumbai"
+
+
+accountEmail="bjbjgb@go.com"
+accountPassword="5386358"
+accountCity="delhi"
+let accountState
+//accountid=1223  //it will give an error if u direct print this so print in table
+console.log(accountid);
+
+
+console.table([accountEmail,accountPassword,accountCity,accountid,accountState])
+
+/*
+please avoid useing var because of use issue in block scope and functional scope
+*/
