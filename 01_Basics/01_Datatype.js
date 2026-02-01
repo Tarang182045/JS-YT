ccbd2d1a26485e68051e08a9b2029b93f7ed3a7c
Tarang Tank <167697814+Tarang182045@users.noreply.github.com>
2026-02-01 07:51:45 +0000

    Datatype

---
+++ 01_Basics/01_Datatype.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as newer version
+
+//alert(3+3)  becuse we r using node js not browser so in browser its ans is 6 but not in node js
+
+let name="Tarang"
+let age = 18
+let isLoggedIn = false
+
+//number  => 2 to power 54
+// bigint
+//string
+//boolean
+//null = standalone value
+//undefined = value is not assignd
+//symbol = for uniqueness
+// object
+
+console.log(typeof name)
