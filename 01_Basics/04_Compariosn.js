2d3e2e7d915e30727980a54bdda87111d0aa6472
Tarang Tank <167697814+Tarang182045@users.noreply.github.com>
2026-02-01 08:03:44 +0000

    comparison and summary

---
+++ 01_Basics/04_Compariosn.js
@@ -0,0 +1,9 @@
+console.log("2">1) // script lang dont allow u to compafre diff datatype becuase u cant find your desire datatype
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)  //equatiy check make nnull to 0 then compare it while >or == cant
+
+
+console.log("2"===2) //its strict check it will not convert the str to num that's why its compare num and datatype
+
---
+++ 01_Basics/Datatype_Summary.js
@@ -0,0 +1,23 @@
+// premitive 
+/* 7 types: String,Number, Boolean,null,undefined,Symbol,BIg int */
+
+
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId)
+
+const bigNumber=127498620524866467n //bigint represenatiion
+
+
+
+// not premitive(refrence)
+/* Array,objects,Functions*/
+
+const heros =["Shaktimann","Thor","Doga"]  //array
+let myobj= {          // obj inside {}
+    name:"Tarang",
+    age:21,
+}
+
