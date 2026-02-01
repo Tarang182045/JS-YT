1424d4fc23d0815adb0bb438515caaf83f205f9a
Tarang Tank <167697814+Tarang182045@users.noreply.github.com>
2026-02-01 08:22:13 +0000

    Summary

--- 01_Basics/Datatype_Summary.js
+++ 01_Basics/Datatype_Summary.js
@@ -21,3 +21,25 @@ let myobj= {          // obj inside {}
     age:21,
 }
 
+
+
+//*********************** *//
+//stack (primitive) and heap (non-primtive)
+
+let MyYoutubename = "Tarangtank.com"
+let othername = MyYoutubename
+othername="Tanktarang"
+
+console.log(MyYoutubename)      // here its give direct copy of data not its refrence value in stack
+console.log(othername)
+
+let userOne = {
+    email:"123@gmail.com",
+    upi:"abc@ybl"
+}
+                          //here memory store in heap where its give direct refrence not its copy
+let userTwo = userOne
+userTwo.email="Tarang@gppgle.com"
+
+console.log(userOne.email)  
+console.log(userTwo.email)
