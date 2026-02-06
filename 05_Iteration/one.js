18d85b4edfe0526798aa3e33b97e7256a2e50bf3
Tarang Tank <167697814+Tarang182045@users.noreply.github.com>
2026-02-06 15:21:07 +0000

    for

---
+++ 05_Iteration/one.js
@@ -0,0 +1,58 @@
+// for
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let i = 0; i <=10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         // console.log("5 the best")
+//     }
+//     // console.log(element)
+
+    
+// for (let i=0; i<=10; i++){
+//     console.log(`outer loop value: ${i}`);
+//     for(let j=0; j<=10; j++){
+//         // console.log(`inner loop value ${j} and inner loop ${i}`);
+         
+//         console.log(i + "*" + j + "=" + i*j)
+
+//     }   
+// }
+// }
+
+// let myArray = ["flash","Tarang","Trash","Frash"]
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element)
+    
+// }
+
+
+// continue and break
+
+// for (let i = 0; i < 20; i++) {
+//     if (i == 5){
+//         console.log("Detected 5")
+//         break;
+//     }
+//     console.log(i)
+    
+// }
+for (let i = 0; i < 20; i++) {
+    if (i == 5){
+        console.log("Detected 5")
+        continue;
+    }
+    console.log(i)
+    
+}
+
+
+
+
+
+
