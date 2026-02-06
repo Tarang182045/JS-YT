f2c2f582dc8581f054fdf59f69240718de72084a
Tarang Tank <167697814+Tarang182045@users.noreply.github.com>
2026-02-06 15:22:02 +0000

    while

---
+++ 05_Iteration/two.js
@@ -0,0 +1,22 @@
+/// while
+
+// let index = 0
+// while(index<=10){
+//     console.log(`value of index is ${index}`)
+//     index = index+2
+// }
+
+// let myArray = ["Flash","Tarang","Iron man"]
+
+// let arr = 0
+// while(arr<myArray.length){
+//     console.log(`I'm ${myArray[arr]}`)
+//     arr++
+// }
+
+let score = 1
+do{
+    console.log(`The number is ${score}`)
+    score++
+
+}while(score<=10)
